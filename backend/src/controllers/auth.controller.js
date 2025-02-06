c5898853cc7f4248244e30cd14d84354ddd69dac
Xaneliar <adi8103289571@gmail.com>
2025-02-06 15:09:29 +0530

    Added Login, Signup and Logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,82 @@
-export const signup = (req,res) => {
-    res.send("signup route");
+import { decrypt } from "dotenv";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req,res) => {
+    const{ fullName,email,password,profilePic } = req.body
+    try {
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "Please fill all the fields"})
+        }
+        if (password.length<6){ res.send(400).json({message: "Password must be atleast 6 characters!"});}
+        const user = await User.findOne({email})
+        if (user) return res.status(400).json({message: "email already exists!"});
+
+        //hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashPass = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashPass,
+        })
+
+        if(newUser){
+            //generate JWT Tokens
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
+        } else {
+            res.status(400).json({message: "Invalid user data"});
+        }
+    } catch (error) {
+        console.log("Error in signup controller: ", error.message);
+        res.status(500).json({message: "Internel Server Error"})
+    }
 };
 
-export const login = (req,res) => {
-    res.send("login route");
+export const login = async (req,res) => {
+    const { email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+
+        if (!user){ return res.status(400).json({message: "Invalid credentials"})}
+
+        const passCheck = await bcrypt.compare(password, user.password)
+        if (!passCheck) { return res.status(400).json({ message: "Invalid Credentials"})}
+        
+        generateToken(user._id,res)
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+    } catch (error) {
+        console.log("Error in login controller: ", error.message);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
 };
 
 export const logout = (req,res) => {
-res.send("logout route");
+    try {
+        res.cookie("jwt", "", {maxAge: 0})
+        res.status(200).json({message: "Logged out successfully!"})
+    } catch (error) {
+        console.log("Error in logout conttroller", error.message);
+        res.status(400).json({message: "Inernal Server Error"})
+    }
 };
+
+export const updateProfile = async (req, res) => {
+    
+}
